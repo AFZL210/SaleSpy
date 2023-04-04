2943c0f84368677e1903e2a2843d04fe3bf67fe1
AFZL210 <ak341668@gmail.com>
2023-04-04 19:38:34 +0530

    init

---
+++ config.js
@@ -0,0 +1,10 @@
+export const AMAZON_URL = '';
+export const FLIPKART_URL = '';
+export const DESIRED_PRICE = 10000;
+export const CHECK_INTERVAL = 100000;
+
+export const CHECK_FOR = "AMAZON/FLIPKART";
+
+export const EMAIL = "xyz@abc.com";
+export const COUNTRY_CODE = "+91";
+export const PHONE_NUMBER = "98xxxxxxxx";
