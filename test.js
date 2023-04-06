dec609685494774ff85740e96cb2f60430a2a04e
Mohd Afzal Khan <79896602+AFZL210@users.noreply.github.com>
2023-04-06 16:26:57 +0000

    removed test.js

--- test.js
+++
@@ -1,8 +0,0 @@
-import wbm from 'wbm'
-
-wbm.start().then(async () => {
-    const phones = [918700209857];
-    const message = 'Good Morning.';
-    await wbm.send(phones, message);
-    await wbm.end();
-}).catch(err => console.log(err));
